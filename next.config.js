d8720ff50ab7141a8f9ae2f30a65aaa15e377362
Nekruz <nekruz.avgani@gmail.com>
2023-05-13 22:18:14 +0100

    config: nextjs config reseted

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   async rewrites() {
     return [{
       source: '/:any*',
