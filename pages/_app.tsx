fe22cb2e77ee9eed6e30f0caf07ac8ed3f236822
Nekruz <nekruz.avgani@gmail.com>
2023-05-20 22:04:37 +0100

    fix: cache is fixed !!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,21 @@
+import React from "react"
 import type { AppProps } from "next/app"
-import { useEffect, useState } from "react"
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { Hydrate } from "react-query/hydration"
 import { ReactQueryDevtools } from "react-query/devtools"
 import "@/styles/globals.css"
 import { CssBaseline } from "@mui/material"
 
 export default function App({ Component, pageProps }: AppProps) {
-	const queryClient = new QueryClient()
-	const [render, setRender] = useState(false)
-	useEffect(() => {
-		setRender(true)
-	}, [])
+	const queryClient = React.useRef(new QueryClient())
 
-	return render ? (
-		<QueryClientProvider client={queryClient}>
+	return (
+		<QueryClientProvider client={queryClient.current}>
 			<CssBaseline />
 			<Hydrate state={pageProps.dehydratedState}>
 				<Component {...pageProps} />
 			</Hydrate>
 			<ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
 		</QueryClientProvider>
-	) : null
+	)
 }
