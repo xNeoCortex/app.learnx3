87b191962d5a95cf31e84e001494c9570d0975bc
Nekruz <nekruz.avgani@gmail.com>
2023-06-22 20:40:08 +0100

    feat: CompletedAssessment component

--- components/assessment/CompletedAssessment.tsx
+++ components/assessment/CompletedAssessment.tsx
@@ -3,46 +3,48 @@ import Image from "next/image"
 import { useRouter } from "next/router"
 import { Alert, Box, Button, Container } from "@mui/material"
 
-function CompletedAssessment(props) {
+function CompletedAssessment({ score, handleButton = null }) {
 	const { back } = useRouter()
+	const handleBack = () => back()
 	return (
-		<Container sx={{ padding: "20px 5px" }}>
-			<Box
-				sx={{
-					background: "#bdbdbd33",
-					margin: "15px ",
-					padding: "0px 0px 20px",
-					borderRadius: 3,
-					display: "flex",
-					justifyContent: "space-around",
-					alignItems: "center",
-					flexDirection: "column",
+		<Box
+			sx={{
+				background: "#bdbdbd33",
+				margin: "15px ",
+				padding: "0px 0px 20px",
+				borderRadius: 3,
+				display: "flex",
+				justifyContent: "space-around",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<Alert
+				severity="success"
+				sx={{ p: 1, m: 2, paddingY: "0px", fontSize: 19, fontWeight: 600, width: "fit-content" }}
+			>
+				You have completed this assessment!
+			</Alert>
+			<h4 style={{ padding: 8, border: "3px solid #999bff", borderRadius: 8, background: "white" }}>
+				⭐️ Your score: {score?.map((x, index) => Math.round(x.result) + (score?.length !== index + 1 && `, `))}
+			</h4>
+			<Image alt="complete image" src="/completed-icon.svg" width={400} height={300} style={{ margin: "auto" }} />
+			<Button
+				onClick={handleButton || handleBack}
+				variant="contained"
+				style={{
+					borderRadius: 20,
+					background: "white",
+					color: "black",
+					width: 200,
+					fontWeight: 600,
+					boxShadow: "none",
+					marginRight: 20,
 				}}
 			>
-				<Alert
-					severity="success"
-					sx={{ p: 1, m: 2, paddingY: "0px", fontSize: 19, fontWeight: 600, width: "fit-content" }}
-				>
-					You have already completed the following assessment!
-				</Alert>
-				<Image alt="complete image" src="/completed-icon.svg" width={400} height={300} style={{ margin: "auto" }} />
-				<Button
-					onClick={() => back()}
-					variant="contained"
-					style={{
-						borderRadius: 20,
-						background: "white",
-						color: "black",
-						width: 200,
-						fontWeight: 600,
-						boxShadow: "none",
-						marginRight: 20,
-					}}
-				>
-					Back
-				</Button>
-			</Box>
-		</Container>
+				Back
+			</Button>
+		</Box>
 	)
 }
 
