a1707da0deb2f18f31bf1ac5d1ab1cd4a951e114
Nekruz <nekruz.avgani@gmail.com>
2023-07-22 09:39:40 +0100

    fix: updated _error message

--- pages/_error.js
+++ pages/_error.js
@@ -3,7 +3,7 @@ function Error({ statusCode }) {
       <p>
         {statusCode
           ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
+          : 'An error occurred. Please refresh your browser or try again later.'}
       </p>
     )
   }
