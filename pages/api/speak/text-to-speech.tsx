fbe9e9229acaec1a99eb6a53023a5fc1b9d4a096
Nekruz <nekruz.avgani@gmail.com>
2023-07-25 12:39:54 +0100

    feat: google text to speech

---
+++ pages/api/speak/text-to-speech.tsx
@@ -0,0 +1,43 @@
+// Imports the Google Cloud client library
+import textToSpeech from "@google-cloud/text-to-speech"
+
+// Import other required libraries
+import fs from "fs"
+import util from "util"
+// Creates a client
+
+export default async function handler(req, res) {
+	if (req.method === "GET") {
+		const { id } = req.query
+
+		try {
+			const client = new textToSpeech.TextToSpeechClient()
+			// The text to synthesize
+			const text = "hello, how are you!"
+
+			// Construct the request
+			const request = {
+				input: { text: text },
+				// Select the language and SSML voice gender (optional)
+				voice: { languageCode: "en-US", ssmlGender: "FEMALE" },
+				// select the type of audio encoding
+				audioConfig: { audioEncoding: "MP3" },
+			}
+
+			// Performs the text-to-speech request
+			//@ts-ignore
+			const [response] = await client.synthesizeSpeech(request)
+			// Write the binary audio content to a local file
+			const writeFile = util.promisify(fs.writeFile)
+			await writeFile("output.mp3", response.audioContent, "binary")
+			console.log("Audio content written to file: output.mp3")
+
+			res.status(200).json(response)
+		} catch (error) {
+			console.error("Error getting student data: ", error)
+			res.status(500).json({ message: "Internal server error" })
+		}
+	} else {
+		res.status(405).json({ message: "Method not allowed" })
+	}
+}
