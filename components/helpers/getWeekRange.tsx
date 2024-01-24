44f0877d31b07844fb0a194945e75985fa81177b
Nekruz <nekruz.avgani@gmail.com>
2024-01-24 20:48:11 +0000

    moved 'getWeekRange' function into a dedicated file for improved organization and maintainability

---
+++ components/helpers/getWeekRange.tsx
@@ -0,0 +1,35 @@
+import { ConvertToWeeklyObjectType, LessonTimetableType } from "@/types/types"
+import dayjs from "dayjs"
+
+function getWeekRange(date: string | Date) {
+	const currentDate = dayjs(date)
+	const dayOfWeek = currentDate.day()
+	const diff = currentDate.date() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1) // Adjust for Sunday
+	const startOfWeek = currentDate.date(diff)
+	const endOfWeek = currentDate.date(diff + 6)
+	return { start: startOfWeek.format("YYYY-MM-DD"), end: endOfWeek.format("YYYY-MM-DD") }
+}
+
+export default function groupLessonsByWeek(input: LessonTimetableType[]): ConvertToWeeklyObjectType[] {
+	const weeklyData: any = {}
+
+	input?.forEach((lesson) => {
+		const lessonDate = new Date(lesson.lesson_date)
+		const weekRange: {
+			start: string
+			end: string
+		} = getWeekRange(lessonDate)
+
+		if (!weeklyData[weekRange.start]) {
+			weeklyData[weekRange.start] = {
+				date_from: weekRange.start,
+				date_to: weekRange.end,
+				lessons: [],
+			}
+		}
+
+		weeklyData[weekRange.start].lessons.push(lesson)
+	})
+
+	return Object.values(weeklyData)
+}
