3561813a24362b16a899d6e296699a6d648279ab
Nekruz <nekruz.avgani@gmail.com>
2024-06-04 16:29:09 +0100

    chore: refactored ProtectedRoute

--- components/auth/ProtectedRoute.tsx
+++ components/auth/ProtectedRoute.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/router"
 import ErrorPage from "../../pages/errorpage"
 import WaitingPage from "../other/WaitingPage"
 import { useStoreUser } from "../zustand"
+import ApiServices from "@/pages/api/ApiServices"
+import { useQuery } from "@tanstack/react-query"
+import { getAuth } from "firebase/auth"
 
 type PermitType = "admin" | "teacher" | "student"
 
@@ -11,7 +14,21 @@ interface ProtectedRouteProps {
 	permitArray: PermitType[]
 }
 const ProtectedRoute = ({ children, permitArray = [] }: ProtectedRouteProps) => {
-	const { userInfo } = useStoreUser()
+	const { userInfo, setUserInfo } = useStoreUser()
+	const userAuth = getAuth().currentUser
+
+	const { fetchStudentData } = ApiServices()
+	const { data, isLoading, isError } = useQuery({
+		queryKey: [`student-${userAuth?.uid}`, String(userAuth?.uid)],
+		queryFn: () => fetchStudentData(String(userAuth?.uid)),
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		enabled: !!userAuth?.uid,
+		onSuccess: (data) => setUserInfo({ ...userInfo, permit: data.data.permit }),
+		onError: (error) => console.error("Error fetching user data:", error), // Log error
+	})
+
+	if (isError) return <ErrorPage />
 
 	if ((!userInfo?.permit && userInfo?.role == "teacher") || (!userInfo?.permit && userInfo?.role == "student"))
 		return <WaitingPage />
