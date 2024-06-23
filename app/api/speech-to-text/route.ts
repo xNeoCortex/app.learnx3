89503fb326ab2e2bd450e205611e08f260d102a7
Nekruz <nekruz.avgani@gmail.com>
2024-06-23 15:40:17 +0100

    feat: add deepgram/sdk for speech to text solution

--- app/api/speech-to-text/route.ts
+++ app/api/speech-to-text/route.ts
@@ -1,61 +1,33 @@
-import OpenAI from "openai"
-import fs from "fs"
-import path from "path"
-import os from "os"
 import { NextRequest, NextResponse } from "next/server"
-import axios from "axios"
+import { createClient } from "@deepgram/sdk"
 
-const openai = new OpenAI({
-	apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-})
+const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY ?? "")
 
 export async function POST(request: NextRequest) {
-	let tempFilePath
 	try {
 		const { myFilePath } = await request.json()
 
-		// Fetch the audio file from the provided URL
-		const response = await axios.get(myFilePath, { responseType: "arraybuffer" })
-
-		// Save the fetched audio file to a temporary file
-		tempFilePath = path.join(os.tmpdir(), `temp-audio-${Date.now()}.mp3`)
-		fs.writeFileSync(tempFilePath, Buffer.from(response.data), "binary")
-
-		// Validate file extension
-		const fileName = path.basename(tempFilePath)
-		const extension = path.extname(fileName).toLowerCase()
-		if (![".mp3", ".mp4", ".mpeg", ".mpga", ".oga", ".ogg", ".wav", ".webm"].includes(extension)) {
-			throw new Error("Unsupported audio format")
-		}
-
-		// Read the file to verify its content
-		const fileContent = fs.readFileSync(tempFilePath)
-		console.log(`File size: ${fileContent.length} bytes`)
-
-		// Generate transcription using OpenAI
-		const transcription = await openai.audio.transcriptions.create({
-			file: fs.createReadStream(tempFilePath),
-			model: "whisper-1",
-		})
-
-		// Clean up the temporary file
-		fs.unlinkSync(tempFilePath)
+		console.log("myFilePath :>> ", myFilePath)
+		// STEP 2: Call the transcribeUrl method with the audio payload and options
+		const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+			{
+				url: myFilePath,
+			},
+			{
+				model: "nova-2",
+				smart_format: true,
+			}
+		)
 
 		return NextResponse.json(
 			{
-				text: transcription.text,
+				text: result?.results.channels[0].alternatives[0].transcript ?? "No transcription available",
+				error,
 			},
 			{ status: 200 }
 		)
 	} catch (error) {
 		console.error("Error generating the audio file:", error)
-
-		// Clean up the temporary file in case of an error
-		if (tempFilePath && fs.existsSync(tempFilePath)) {
-			fs.unlinkSync(tempFilePath)
-		}
-
-		// Return a more informative error message
-		return NextResponse.json({ message: error, tempFilePath }, { status: 500 })
+		return NextResponse.json({ message: error }, { status: 500 })
 	}
 }
--- app/components/Chat/SpeakAI.tsx
+++ app/components/Chat/SpeakAI.tsx
@@ -1,8 +1,6 @@
 "use client"
 import { useChatStore, useStoreFilePath, useSuggestionsStore } from "../zustand"
 import axios from "axios"
-import OpenAI from "openai"
-import { getFunctions, httpsCallable } from "@firebase/functions"
 
 function useSpeakAI() {
 	const { setAiFilePath } = useStoreFilePath()
@@ -15,40 +13,12 @@ function useSpeakAI() {
 		try {
 			setAiFilePath(null)
 
-			const audioFunc = httpsCallable(getFunctions(), "downloadaudio")
-
-			const { data } = await audioFunc({ audioFilePath: myFilePath })
-
-			// Decode the base64 encoded audio data
-			//@ts-ignore
-			const base64Audio = data.data // Base64 encoded audio data
-			const byteCharacters = atob(base64Audio)
-			const byteNumbers = new Array(byteCharacters.length)
-			for (let i = 0; i < byteCharacters.length; i++) {
-				byteNumbers[i] = byteCharacters.charCodeAt(i)
-			}
-			const byteArray = new Uint8Array(byteNumbers)
-
-			// Create a Blob from the Uint8Array
-			const blob = new Blob([byteArray], { type: "audio/mp3" })
-
-			const formData = new FormData()
-			formData.append("model", "whisper-1")
-			formData.append("file", blob, "audio.mp3") // Provide a filename for the audio file
-			formData.append("response_format", "text")
-
-			// Send the request to OpenAI using fetch
-			const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
-				headers: {
-					Authorization: "Bearer " + process.env.NEXT_PUBLIC_OPENAI_KEY,
-				},
-				method: "POST",
-				body: formData,
+			const { data } = await axios.post("/api/speech-to-text", {
+				myFilePath,
 			})
-			const voiceText = await res.text()
 
 			// Add the user message to the chat
-			messageFromGPT = [...messages, { role: "user", content: voiceText }]
+			messageFromGPT = [...messages, { role: "user", content: data.text }]
 			addMessage(messageFromGPT)
 			addSuggestion("")
 
