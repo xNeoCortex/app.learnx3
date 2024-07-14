02ad5c372c0119105fe6b84a22c3739f1e57bf69
Nekruz <nekruz.avgani@gmail.com>
2024-07-14 19:23:18 +0100

    refactor: Add Translator component for text translation

---
+++ app/components/elements/Translator.tsx
@@ -0,0 +1,89 @@
+"use client"
+import { useState } from "react"
+import { Box, IconButton, Typography } from "@mui/material"
+import axios from "axios"
+import GTranslateIcon from "@mui/icons-material/GTranslate"
+import { brandColors } from "../utils/brandColors"
+import { TailSpin } from "react-loading-icons"
+
+export default function Translator({
+	text,
+	fontSize,
+	fontColor,
+	iconColor,
+	flexDirection,
+	children,
+}: {
+	text: string
+	fontSize?: number | string
+	fontColor?: string
+	iconColor?: string
+	flexDirection?: "row" | "column"
+	children: any
+}) {
+	const [targetLanguage, setTargetLanguage] = useState("Russian")
+	const [translatedText, setTranslatedText] = useState("")
+	const [propText, setPropText] = useState("")
+	const [loading, setLoading] = useState(false)
+
+	const handleTranslate = async () => {
+		setLoading(true)
+		setPropText(text)
+		try {
+			const response = await axios.post("/api/translate", { text, targetLanguage })
+			const data = response.data
+			setTranslatedText(data.translatedText)
+			setLoading(false)
+		} catch (error) {
+			console.error("Error translating text:", error)
+			setLoading(false)
+			setTranslatedText("Failed to translate text")
+		}
+	}
+
+	return (
+		<Box
+			//@ts-ignore
+			display={text ? "flex" : "none"}
+			flexDirection={flexDirection ?? "column"}
+			gap={"4px"}
+			mt={"6px"}
+			maxWidth={"100%"}
+		>
+			<Box display={"flex"} alignItems={"center"} maxWidth={"100%"} flex={4}>
+				<IconButton onClick={handleTranslate}>
+					<GTranslateIcon sx={{ color: iconColor ?? "#060634", width: "22px" }} />
+				</IconButton>
+				{children}
+			</Box>
+			<Box
+				display={"flex"}
+				flexDirection={flexDirection ?? "row"}
+				gap={1}
+				maxWidth={"100%"}
+				flex={3}
+				padding={"0px 2px"}
+			>
+				{loading ? (
+					<TailSpin style={{ width: "20px" }} stroke={brandColors.darkBlue} />
+				) : (
+					translatedText && (
+						<Box
+							display={propText !== text ? "none" : "flex"}
+							width={"100%"}
+							alignItems={"center"}
+							border={"1px solid #E0E0E0"}
+							borderRadius={"8px"}
+							bgcolor={"#F9F9F9"}
+							padding={"4px 8px"}
+						>
+							<Typography fontSize={fontSize ?? "inherit"} color={fontColor ?? brandColors.darkerGrey}>
+								{translatedText}
+							</Typography>
+						</Box>
+					)
+				)}
+			</Box>
+		</Box>
+	)
+}
