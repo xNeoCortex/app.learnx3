f7afe282efbe545462e8a05779fee5b6100fcf44
Nekruz <nekruz.avgani@gmail.com>
2024-07-30 00:05:22 +0100

    refactor: Add filterLessonsBySubscriptionType helper function

---
+++ app/components/helpers/filterLessonsBySubscriptionType.tsx
@@ -0,0 +1,11 @@
+import { LessonTimetableType, UserType } from "@/types/types"
+import isDateBeforeToday from "./isDateBeforeToday"
+
+export const filterLessonsBySubscriptionType = (lessons: LessonTimetableType[], userInfo: UserType) => {
+	if (userInfo?.subscription_type === "pro" || userInfo?.role === "teacher" || userInfo?.role === "admin") {
+		return lessons.filter((lesson) => !isDateBeforeToday(lesson.lesson_date as string))
+	}
+	return lessons.filter(
+		({ lesson_date, lesson_type }) => !isDateBeforeToday(lesson_date as string) && lesson_type === "speaking_club"
+	)
+}
